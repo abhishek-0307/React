65aa730cf11915efca03549abc64651dab760317
unknown <abhishek.bharmavath@gmail.com>
2024-02-14 22:45:52 +0530

    jsx-table

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import OrderList from "./components/functional-components/list/order-list/order-
 import UnorderList from "./components/functional-components/list/unorder-list/unorder-list";
 import Image from "./components/functional-components/image/image";
 import TableComponent from "./components/functional-components/table/table";
+import Profile from "./components/functional-components/jsx/jsx";
 function App() {
   return (
     
@@ -10,11 +11,7 @@ function App() {
         <h1>
           Hello World
         </h1>
-        <ButtonComponent/>
-        <OrderList/>
-        <UnorderList/>
-        <Image/>
-        <TableComponent/>
+        <Profile/>
         
         </div>
       
